8bdfc4e5aef98c284bdf9fbd61877dfe704c7c27
MmakgadiLetsela <mmakgadi.letsela@gmail.com>
2026-03-09 16:34:26 +0200

    add projects function including two previously completed projects

---
+++ src/components/Projects.jsx
@@ -0,0 +1,48 @@
+
+
+
+
+export default function Projects() {
+
+    const projects = [
+        {
+            id: 1,
+            title: "Kanban Tasks Management App",
+            description: "A web application that allows users to manage their tasks using a Kanban board. Users can create, edit, and delete tasks, as well as move them between different columns (e.g., To Do, In Progress, Done).",
+            technologies: ["Javascript", "HTML", "CSS"],
+            githubLink: "https://github.com/MmakgadiLetsela/MMALET25583_FTO2506_Group-B_Mmakgadi-Letsela_JSLPP.git",
+            image: "./assets/kanban-app.png",
+
+        },
+
+        {
+            id: 2,
+            title: "React Podcast Landing Page",
+            description: "A responsive landing page for a podcast, built using React. The page includes sections for the podcast's description, episodes, and the image.",
+            technologies: ["React", "HTML", "CSS"],
+            githubLink: "https://github.com/MmakgadiLetsela/MMALET25583_PTO2505_Group_A_Mmakgadi-Letsela_DJS03.git",
+            image: "./assets/podcast-landing-page.png",
+        }
+    ]
+ }
+
+ return(
+    <section id="projects" classname="projects">
+        <div className="container">
+            <h2>My Projects</h2>
+            <div className="projects-grid">
+                {projects.map(project => (
+                    <div key={project.id} className="project-card">
+                        <img src={project.image} alt={`${project.title}-image`} className="project-image" />
+                        <div className="project-info">
+                            <h3>{project.title}</h3>
+                            <p>{project.description}</p>
+                            <p><strong>Technologies:</strong> {project.technologies.join(", ")}</p>
+                            <a href={project.githubLink} target="_blank" rel="noopener noreferrer" className="github-link">View on GitHub</a>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </div>
+    </section>
+ )
